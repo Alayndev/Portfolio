113400d8d8fb49d625953845880efed91ec1b742
AlaynDev <saezalayn@gmail.com>
2022-04-22 19:25:31 -0300

    Link fixed

--- src/components/ContactSocialMedia/ContactSocialMedia.jsx
+++ src/components/ContactSocialMedia/ContactSocialMedia.jsx
@@ -3,24 +3,27 @@ import React from "react";
 export function ContactSocialMedia() {
   return (
     <>
-      <div className='share'>
+      <div className="share">
         <a
-          href='https://www.linkedin.com/'
-          target='_blank'
-          rel='noreferrer'
-          className='fab fa-linkedin'></a>
-          
-        <a
-          href='https://github.com/Alayndev'
-          target='_blank'
-          rel='noreferrer'
-          className='fab fa-github'></a>
+          href="https://www.linkedin.com/in/alayn-saez/"
+          target="_blank"
+          rel="noreferrer"
+          className="fab fa-linkedin"
+        ></a>
 
         <a
-          href='https://www.instagram.com/alayn.saez/'
-          target='_blank'
-          rel='noreferrer'
-          className='fab fa-instagram'></a>
+          href="https://github.com/Alayndev"
+          target="_blank"
+          rel="noreferrer"
+          className="fab fa-github"
+        ></a>
+
+        <a
+          href="https://www.instagram.com/alayn.saez/"
+          target="_blank"
+          rel="noreferrer"
+          className="fab fa-instagram"
+        ></a>
       </div>
     </>
   );
