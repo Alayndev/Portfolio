5711148d26ed48704053771f198a7a2c818c12f0
AlaynDev <saezalayn@gmail.com>
2022-07-28 13:40:06 -0300

    update

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -9,8 +9,8 @@ export function ContactForm() {
 
   if (state.succeeded) {
     return (
-      <div className='home'>
-        <div className='content'>
+      <div className="home">
+        <div className="content">
           <h3>Thank you for contacting me!</h3> <span>I will reply ASAP!</span>
         </div>
       </div>
@@ -19,34 +19,34 @@ export function ContactForm() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className='inputBox'>
-        <input id='name' type='text' name='name' placeholder='Full Name' />
-        <ValidationError prefix='Name' field='name' errors={state.errors} />
+      <div className="inputBox">
+        <input id="name" type="text" name="name" placeholder="Full Name" />
+        <ValidationError prefix="Name" field="name" errors={state.errors} />
 
-        <input id='email' type='email' name='email' placeholder='Email' />
-        <ValidationError prefix='Email' field='email' errors={state.errors} />
+        <input id="email" type="email" name="email" placeholder="Email" />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
       </div>
 
-      <div className='inputBox'>
-        <input id='subject' type='text' name='subject' placeholder='Subject' />
+      <div className="inputBox">
+        <input id="subject" type="text" name="subject" placeholder="Subject" />
         <ValidationError
-          prefix='Subject'
-          field='subject'
+          prefix="Subject"
+          field="subject"
           errors={state.errors}
         />
 
-        <input id='company' type='text' name='company' placeholder='Company' />
+        <input id="company" type="text" name="company" placeholder="Company" />
         <ValidationError
-          prefix='Company'
-          field='company'
+          prefix="Company"
+          field="company"
           errors={state.errors}
         />
       </div>
 
-      <textarea id='message' name='message' placeholder='Message' />
-      <ValidationError prefix='Message' field='message' errors={state.errors} />
+      <textarea id="message" name="message" placeholder="Message" />
+      <ValidationError prefix="Message" field="message" errors={state.errors} />
 
-      <button type='submit' className='btn' disabled={state.submitting}>
+      <button type="submit" className="btn" disabled={state.submitting}>
         Submit
       </button>
     </form>
